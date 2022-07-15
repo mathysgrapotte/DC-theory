7cf1e46313bb31cefd15b3e11956851bd6190618
jpelloux <julespellouxprayer@gmail.com>
2022-07-15 04:52:09 -0400

    new field in parser (card type, power, thougness,...) and refacto

--- mtg_toolbox.py
+++ mtg_toolbox.py
@@ -17,3 +17,28 @@ def compute_cmc(mana_cost) :
         else :
             cmc += int(mana_matrix[mana])
     return cmc
+
+class Card_Type:
+    
+    supertypes_list = ["Basic", "Legendary", "Ongoing", "Snow", "World"] 
+    def __init__(self, type_line):
+        self.supertypes = []
+        self.card_types = []
+        self.subtypes = []
+        self.parse_type_line(type_line)
+        
+    def parse_type_line(self, type_line) :
+        parts = type_line.split('—')
+        #type and supertype
+        type_and_supertype = parts[0].strip().split(" ")
+        for part in type_and_supertype :
+            if part in self.supertypes_list :
+                self.supertypes.append(part)
+            else :
+                self.card_types.append(part)
+        #subtype
+        if len(parts) == 2 :
+            subtypes = parts[1].strip().split(" ")
+            for subtype in subtypes :
+                self.subtypes.append(subtype)
+        
